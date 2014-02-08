ad99bc7dd7f930c65c25f5efe7706d3da2fd754d
Meg Desko <mdesko@gmail.com>
2014-02-08 10:31:36 -0800

    try to get the app to talk to me

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 
+echo "Hello Team Back2Court!!  I want a db connection";
 # This function reads your DATABASE_URL configuration automatically set by
 # Heroku
 # the return value is a string that will work with pg_connect
 function pg_connection_string() {
 	return "dbname=d9g8fdnb8rv1ci host=ec2-54-197-241-95.compute-1.amazonaws.com port=5432
 user=plszpihaesglmk password=oqrfEGFmeUP9WHMg6sX8rU6T1q sslmode=require";
-
-
 }
  
 # Establish db connection
@@ -18,6 +17,7 @@ if (!$db) {
 }
  
 $result = pg_query($db, "SELECT * from clients");
-
-echo "Hello Team Back2Court!!  I have a db connection"
+if (!$result) {
+	echo "I have no results :-(";
+}
 ?>
