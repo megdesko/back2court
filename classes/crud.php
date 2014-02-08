b6d93c80acafaa6c99353f258e3879e005c81aac
Meg Desko <mdesko@gmail.com>
2014-02-08 13:51:45 -0800

    adding crud

---
+++ classes/crud.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * @abstract crud class to deal with db CRUD operations
+ * @author megdesko
+ * @copyright Back2Court
+ * 
+ */
+
+
+include("./dbconnect.php");
+
+class crud extends dbconnect{
+
+var $table;
+
+public function __construct() {
+	parent::__construct();
+}
+
+/**
+ * @abstract create new record in the table
+ * @param array $post // contains the data that we need to insert
+ *
+ */ 
+
+public function create($post) {
+	
+	$sql = "INSERT INTO $this->table  SET ";
+
+	foreach ($post as $column => $value) {
+		$sql .= $column . " = '" . $value . "'";	
+	}	
+	$result = pg_query($this->dbconn, $sql);
+
+	return ($result);
+	
+}
+
+public function get_one($id) {
+
+	$sql = "SELECT * FROM $this->table WHERE ";
+	$sql .= $this->table_id  . "_id = '$id'";
+	$result = pg_query($this->dbconn, $sql);
+
+	return ($result);
+
+
+}
+
+public function get_all() {
+
+	$sql = "SELECT * FROM $this->table";
+	$result = pg_query($this->dbconn, $sql);
+
+	return ($result);
+
+}
+
+public function update($data) {
+	if (!$data['client_id']) {return;}
+
+	$sql = "SELECT * FROM $this->table";
+	$result = pg_query($this->dbconn, $sql);
+
+	return ($result);
+
+}
+
+public function delete($id) {
+	// if we don't have an id here, abort so we don't kill the db
+	if (!$id) {return;}
+	
+	$sql = "DELETE FROM $this->table WHERE ";
+	$sql .= $this->table_id  . "_id = '$id'";
+	$result = pg_query($this->dbconn, $sql);
+
+	return ($result);
+
+}
+
+}
+
+?>
