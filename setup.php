e24630526707f5c8c04b324ba43681bf769bef7b
Meg Desko <mdesko@gmail.com>
2014-02-08 12:12:39 -0800

    add db connection for setup

--- setup.php
+++ setup.php
@@ -1,4 +1,12 @@
 <?php
+include(./classes/dbconnect.php);
+
+$connection = new dbconnect();
+
+$db = $connection->db_connect();
+if ($db) echo "I have a connection!";
+else echo "I am sad";
+echo "<br><br>";
 echo "Hello from the setup page!"
 
 ?>
