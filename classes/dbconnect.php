1453442810a6d35be64df621575820aac6a0dd64
Meg Desko <mdesko@gmail.com>
2014-02-08 11:47:11 -0800

    adding classes dir, messing with html a bit

---
+++ classes/dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+
+class dbconnect {
+
+var $dbconn;
+
+public function db_connect() {
+	if (! $this->dbconn) {
+
+		$this->dbconn = pg_connect("host=ec2-54-197-241-95.compute-1.amazonaws.com port=5432
+		dbname=d9g8fdnb8rv1ci user=plszpihaesglmk password=oqrfEGFmeUP9WHMg6sX8rU6T1q
+		sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
+	} 
+	
+	return $this->dbconn;
+}
+
+
+}
+?>
