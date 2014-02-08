fbc9aecf6ba2dab46ad65c744cfb493187e52e2b
Meg Desko <mdesko@gmail.com>
2014-02-08 10:25:10 -0800

    initial db commit

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-echo "Hello World!"
+
+# This function reads your DATABASE_URL configuration automatically set by
+# Heroku
+# the return value is a string that will work with pg_connect
+function pg_connection_string() {
+  // we will fill this out next
+}
+ 
+# Establish db connection
+$db = pg_connect(pg_connection_string());
+if (!$db) {
+    echo "Database connection error."
+    exit;
+}
+ 
+$result = pg_query($db, "SELECT * from clients");
+
+echo "Hello Team Back2Court!!  I have a db connection"
 ?>
