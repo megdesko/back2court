62f7e850c825cebf597cdaad4cfe3fc1d6e7560a
Meg Desko <mdesko@gmail.com>
2014-02-08 12:37:08 -0800

    try to create a table

--- setup.php
+++ setup.php
@@ -8,6 +8,19 @@ $connection = new dbconnect();
 if ($connection->dbconn) echo "I have a connection!";
 else echo "I am sad";
 echo "<br><br>";
-echo "Hello from the setup page!"
 
+$sql= "CREATE TABLE clients (
+	client_id   integer,
+	first_name  varchar(50),
+	last_name   varchar(100),
+	phone_1     varchar(15)
+);";
+
+$result = pg_query($connection->dbconn, $sql); 
+
+if ($result) echo "I have a client table<br><br>";
+else echo "client table failed";
+
+echo "<hr>";
+echo "I am finished with my setup routine.";
 ?>
