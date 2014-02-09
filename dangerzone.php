4bb6732c792322f40882b18a0bbe4ed5a816896a
Meg Desko <mdesko@gmail.com>
2014-02-08 20:42:27 -0800

    lots of setup, probably some unnecessary

--- dangerzone.php
+++ dangerzone.php
@@ -44,5 +44,76 @@ $result = pg_query($connection->dbconn, $sql);
 if ($result) echo "Executed $sql<br><br>";
 
 echo "<hr>";
+$sql= "DROP TABLE users;";
+
+$result = pg_query($connection->dbconn, $sql); 
+if ($result) echo "Executed $sql<br><br>";
+
+echo "<hr>";
+$sql= "CREATE TABLE IF NOT EXISTS users (
+	user_id 			serial primary key,
+	phone_number		varchar(15),
+	email_address		varchar(100),
+	first_name			varchar(100),
+	last_name			varchar(100)
+);";
+
+$result = pg_query($connection->dbconn, $sql); 
+if ($result) echo "Executed $sql<br><br>";
+
+echo "<hr>";
+$sql= "DROP TABLE user_clients;";
+
+$result = pg_query($connection->dbconn, $sql); 
+if ($result) echo "Executed $sql<br><br>";
+
+echo "<hr>";
+$sql= "CREATE TABLE IF NOT EXISTS user_clients (
+	user_client_id 			serial primary key,
+	client_id   			integer,
+	user_id		   			integer
+);";
+
+$result = pg_query($connection->dbconn, $sql); 
+if ($result) echo "Executed $sql<br><br>";
+
+echo "<hr>";
+$sql= "DROP TABLE venues;";
+
+$result = pg_query($connection->dbconn, $sql); 
+if ($result) echo "Executed $sql<br><br>";
+
+echo "<hr>";
+$sql= "CREATE TABLE IF NOT EXISTS venues (
+	venue_id 			serial primary key,
+	jurisdiction_id 	integer,
+	name				varchar(250),
+	street_address		varchar(100),
+	city				varchar(100),
+	state				char(2),
+	zip					integer(5)
+);";
+
+$result = pg_query($connection->dbconn, $sql); 
+if ($result) echo "Executed $sql<br><br>";
+
+echo "<hr>";
+$sql= "DROP TABLE jurisdictions;";
+
+$result = pg_query($connection->dbconn, $sql); 
+if ($result) echo "Executed $sql<br><br>";
+
+echo "<hr>";
+$sql= "CREATE TABLE IF NOT EXISTS jurisdictions (
+	name				varchar(250),
+	city				varchar(100),
+	county				varchar(100),
+	state				char(2)
+);";
+
+$result = pg_query($connection->dbconn, $sql); 
+if ($result) echo "Executed $sql<br><br>";
+
+echo "<hr>";
 echo "I am finished with my setup routine.";
 ?>
